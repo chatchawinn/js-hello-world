5e7bc1b1be71f2e2ba198467070169235d665eb3
chatchawinn <BeautCCW@gmail.com>
2022-08-27 04:26:39 +0000

    update message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1>Merry chistmas i โง่</h1>')
+  return res.send('<h1>Happy new years</h1>')
 })
 
 const PORT = process.env.PORT || 3000
